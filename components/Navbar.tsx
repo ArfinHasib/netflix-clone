c75a50060b833124bd52a8ef81986722bb836d53
ArfinHasib <arfinhasib21375@gmail.com>
2023-11-16 00:26:12 +0600

    Building mobile menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,15 @@
 import NavbarItem from './NavbarItem';
+import MobileMenu from './MobileMenu';
 import { BsChevronDown } from 'react-icons/bs';
+import { useCallback, useState } from 'react';
 
 const Navbar = () => {
+   const [showMobileMenu, setShowMobileMenu] = useState(false);
+
+   const toggleMobileMenu = useCallback(() => {
+      setShowMobileMenu((current) => !current);
+   }, []);
+
    return (
       <nav className='w-full fixed z-40'>
          <div className='px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 bg-zinc-900 bg-opacity-90'>
@@ -15,10 +23,15 @@ const Navbar = () => {
                <NavbarItem label='My List' />
                <NavbarItem label='Browse by languages' />
             </div>
-            <div className='lg:Hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative'>
+            <div
+               onClick={toggleMobileMenu}
+               className='lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative'
+            >
                <p className='text-white text-sm'>Browse</p>
                <BsChevronDown className='text-white transition' />
+               <MobileMenu visible={showMobileMenu} />
             </div>
+            <div className=''></div>
          </div>
       </nav>
    );
