7d43ba8ae573ef9fcb2dea968f9df30d9dcf20c9
ArfinHasib <arfinhasib21375@gmail.com>
2023-11-16 08:49:02 +0600

    Completed the navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,49 @@
 import NavbarItem from './NavbarItem';
 import MobileMenu from './MobileMenu';
-import { BsChevronDown } from 'react-icons/bs';
-import { useCallback, useState } from 'react';
+import AccountMenu from './AccountMenu';
+
+import { BsBell, BsChevronDown } from 'react-icons/bs';
+import { useCallback, useEffect, useState } from 'react';
+import { BsSearch } from 'react-icons/bs';
+
+const TOP_OFFSET = 66;
 
 const Navbar = () => {
    const [showMobileMenu, setShowMobileMenu] = useState(false);
+   const [showAccountMenu, setShowAccountMenu] = useState(false);
+   const [showBackground, setShowBackground] = useState(false);
+
+   useEffect(() => {
+      const handleScroll = () => {
+         if (window.scrollY >= TOP_OFFSET) {
+            setShowBackground(true);
+         } else {
+            setShowBackground(false);
+         }
+      };
+
+      window.addEventListener('scroll', handleScroll);
+
+      return () => {
+         window.removeEventListener('scroll', handleScroll);
+      };
+   }, []);
 
    const toggleMobileMenu = useCallback(() => {
       setShowMobileMenu((current) => !current);
    }, []);
 
+   const toggleAccountMenu = useCallback(() => {
+      setShowAccountMenu((current) => !current);
+   }, []);
+
    return (
       <nav className='w-full fixed z-40'>
-         <div className='px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 bg-zinc-900 bg-opacity-90'>
+         <div
+            className={`px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 ${
+               showBackground ? 'bg-zinc-900 bg-opacity-90' : ''
+            }`}
+         >
             {/* eslint-disable-next-line */}
             <img src='/images/logo.png' alt='Logo' className='h-4 lg:h-7' />
             <div className='flex-row ml-8 gap-7 hidden lg:flex'>
@@ -28,10 +59,37 @@ const Navbar = () => {
                className='lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative'
             >
                <p className='text-white text-sm'>Browse</p>
-               <BsChevronDown className='text-white transition' />
+               <BsChevronDown
+                  className={`text-white transition ${
+                     showMobileMenu ? 'rotate-180' : 'rotate-0'
+                  }`}
+               />
                <MobileMenu visible={showMobileMenu} />
             </div>
-            <div className=''></div>
+            <div className='flex flex-row ml-auto gap-7 items-center'>
+               <div className='text-gray-200 hover:text-gray-300 cursor-pointer'>
+                  <BsSearch />
+               </div>
+               <div className='text-gray-200 hover:text-gray-300 cursor-pointer'>
+                  <BsBell />
+               </div>
+
+               <div
+                  onClick={toggleAccountMenu}
+                  className='flex flex-row items-center gap-2 cursor-pointer relative'
+               >
+                  <div className='w-6 h-6 lg:w-10 lg:h-10 rounded-md overflow-hidden'>
+                     {/* eslint-disable-next-line */}
+                     <img src='/images/default-blue.png' alt='Profile' />
+                  </div>
+                  <BsChevronDown
+                     className={`text-white transition ${
+                        showAccountMenu ? 'rotate-180' : 'rotate-0'
+                     }`}
+                  />
+                  <AccountMenu visible={showAccountMenu} />
+               </div>
+            </div>
          </div>
       </nav>
    );
